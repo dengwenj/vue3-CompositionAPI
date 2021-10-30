01372613293cdb5f07f845ce96aa194d0079a49e
xiaodeng <927561237@qq.com>
2021-10-30 14:50:50 +0800

    ref函数

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
   <div>
-    <h2>{{ person.name }}</h2>
-    <h2>{{ person.age }}</h2>
-    <button @click="say">say</button>
+    <div>{{ name }}</div>
+    <div>{{ age }}</div>
+    <div>{{ job.type }}</div>
+    <div>{{ job.sex }}</div>
+    <button @click="uodateClick">点击修改</button>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+/* 
+  ref 函数：
+    作用：定义一个响应式的数据
+    语法：const xxx = ref(value)
+      创建一个包含响应式数据的引用对象
+      js中操作数据：xxx.value
+      模板中读取数据：不需要.value
+
+    接收的数据可以是：基本类型、也可以是对象类型
+    基本类型的数据：响应式依然是靠 Object.defineProperty()的get和set
+    对象类型的数据：内部借助了 Vue3中的新函数——reactive函数
+*/
+import { ref } from 'vue'
 export default {
   name: 'App',
   // 组合式 API 都要写在 setup 里面
   setup() {
-    let person = reactive({
-      name: 'dwj',
-      age: 18,
+    let name = ref('xiaodeng')
+    let age = ref(21)
+    let job = ref({
+      type: 'f2e',
+      sex: '男',
     })
 
-    function say() {
-      alert('say')
+    function uodateClick() {
+      name.value = 'dwj'
+      age.value = 18
+      job.value.type = 'nodejs'
+      job.value.sex = '女'
+      console.log(name, age)
+      console.log(job)
     }
-    // 若返回一个对象，则对象中的属性，方法在模板中均可以直接使用
+
     return {
-      person,
-      say,
+      name,
+      age,
+      job,
+      uodateClick,
     }
   },
 }
