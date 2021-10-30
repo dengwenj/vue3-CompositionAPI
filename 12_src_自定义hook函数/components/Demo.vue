c5d35364e3f148460c9b9255a21954581ffcb3f8
xiaodeng <927561237@qq.com>
2021-10-31 00:01:41 +0800

    自定义hook函数

---
+++ 12_src_自定义hook函数/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2 v-show="person.title">我是{{ person.title }}组件</h2>
+    <h2>{{ person.name }}</h2>
+    <h2>{{ person.age }}</h2>
+  </div>
+</template>
+
+<script>
+import usePerson from '../hooks/usePerson'
+export default {
+  setup() {
+    const person = usePerson('Demo')
+
+    return { person }
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2 v-show="person.title">我是{{ person.title }}组件</h2>
+    <h2>{{ person.name }}</h2>
+    <h2>{{ person.age }}</h2>
+  </div>
+</template>
+
+<script>
+import usePerson from '../hooks/usePerson'
+export default {
+  setup() {
+    const person = usePerson('Test')
+    return {
+      person,
+    }
+  },
+}
+</script>
+
+<style></style>
